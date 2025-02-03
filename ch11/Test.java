e31ed4e61109bed46db0c4e816d8f6b957f6a21f
정성빈 <tjdqls3124@gmail.com>
2025-02-03 14:17:51 +0900

    throws 키워드 : 상위 클래스로 회피

--- ch11/Test.java
+++ ch11/Test.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 //자바는 기본적으로 돌다리도 두드려보고 건너라 개념
 // 생성자나 메소드를 만들 때 발생할 수 이쑨 문제를 예외로 만들고, 그 상황에 대한 대응책도 미리 만들어 둔다.
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         // Error vs Exception
         {
 //            //      m1();
@@ -22,12 +22,22 @@ public class Test {
 
         // 대응 코드
         {
+            // #1 내가 직접 처리 try-catch 로 처리된 m3() 호출
             m3();
+
+            try {
+                m4();
+            }catch (FileNotFoundException e) {
+                System.out.println("예외 처리");
+            } finally {
+                System.out.println("항상 수행");
+            }
         }
     }
 // Exception in thread "main" java.lang.StackOverflowError
-    static void m() {
-        m();
+    static void m1() {
+        m1();
+
     }
 // Exception in thread "main" java.lang.NullPointerException
 // compile error X
@@ -44,19 +54,21 @@ public class Test {
 //    }
 
     // 대응코드를 포함한 코드
-    static void m3() {
+    static void m3() throws FileNotFoundException {
         //throws FileNotFoundException 로 먼둘어진 메소드를 호출 할 때 만약 파일이 없으면 어떻게 대웅?
         // 대응책이 없으면 문법 오류 발생
         // #1 내가 직접 처리
-        try {
-            FileInputStream fis = new FileInputStream("readme.txt");
-            fis.read();
-            System.out.println("정상 수행");
-        } catch (IOException e) {
-            System.out.println("파일이 없으면 이렇게 한다.");
-        } finally {
-            System.out.println("항상 수행된다.");
-        }
+//        try {
+//            FileInputStream fis = new FileInputStream("readme.txt");
+//            System.out.println("정상 수행");
+//        } catch (IOException e) {
+//            System.out.println("파일이 없으면 이렇게 한다.");
+//        } finally {
+//            System.out.println("항상 수행된다.");
+//        }
+
+        // #2 회피 : 나를 호출한 곳으로 toss
+        FileInputStream fis = new FileInputStream("readme.md");
     }
 
     static void m4() throws FileNotFoundException {
