797c1ae74bb6c8617276251d0f7518643b737112
정성빈 <tjdqls3124@gmail.com>
2025-02-04 10:32:39 +0900

    복수개의 제네릭 추가

--- ch13/Test.java
+++ ch13/Test.java
@@ -8,5 +8,14 @@ public class Test {
 
         Box<Integer> box2 = new Box<>();
         box2.content = 10;
+
+        // 복수개의 generic
+        Product<TV, String> product1 = new Product<>();
+        product1.setKind(new TV());
+        product1.setModel("OLED");
+
+        Product<Car, String> product2 = new Product<>();
+        product2.setKind(new Car());
+        product2.setModel("제네시스");
     }
 }
