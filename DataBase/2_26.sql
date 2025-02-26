8739fe9a3ca3eee5318221ac3f0f7b090482c6d5
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-02-26 11:46:24 +0900

    Add files via upload

--- DataBase/2_26.sql
+++ DataBase/2_26.sql
@@ -51,3 +51,17 @@ select custid, count(*) as book_count from orders where saleprice >= 8000 group 
 
 -- group by select 컬럼 주의
 select bookid, count(*) as '도서수량' from orders where saleprice >= 8000 group by custid having count(*) >= 2; -- group by 항목이 아닌 항목을 select에 사용
+
+desc country;
+
+-- world db
+-- 1. country 테이블에서 유럽(Europe) 대륙에 속하는 모든 국가의 인구수(Population) 의 총합은?
+select sum(Population) as'총합' 
+from country
+where Continent = 'Europe';
+
+-- 2. conutry 테이블에서 대륙(Continent)별 건수, 최대인구수, 최소 Gnp, 최대 Gnp, 평균 기대수명을 구하시오
+select Continent, count(*) cnt, max(Population), min(GNP), max(GNP), avg(LifeExpectancy)
+from country
+group by Continent;
+
