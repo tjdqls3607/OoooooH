7dbbaa005e1eae8e80f77be1be69634f271992d9
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-03-10 14:38:15 +0900

    DeadLock

--- DataBase/3-10.sql
+++ DataBase/3-10.sql
@@ -17,6 +17,33 @@ commit;
 delete from customer where id = 5;
 commit;
 
-set autocommit = 1;
+set autocommit = 0;
 
 delete from customer where id = 4;
+
+-- 동시성 제어(쓰기, 쓰기)
+-- madang schema
+-- Lock은 row 단위로 처리
+select * from book where bookid = 3;
+
+select @@autocommit;
+set autocommit = 0;
+
+start transaction;
+
+update book set price = 2000 where bookid = 4;
+
+-- 다른 세션에서 commit이 발생한 경우, 자동으로 commit 된 데이터가 바로 보이지 않고, 한번 더 commit을 수행해야 보인다.
+commit;
+
+-- 데드락 (Dead Lock)
+-- 1, 2 book에 대해서 테스트	
+select * from book where bookid = 3;
+
+start transaction;
+
+update book set price = 5000 where bookid = 3;	-- 1번 lock
+
+update book set price = 4000 where bookid = 4;	-- 2번 lock
+
+commit;
