6b2610faeb228203223a65d527680ddd4e2358f8
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-03-11 14:53:11 +0900

    Add files via upload

--- DataBase/3-11(1).sql
+++ DataBase/3-11(1).sql
@@ -38,3 +38,58 @@ select job_id, sum(salary) from employees group by job_id;
 select job_id, sum(salary) from employees group by job_id with rollup;
 select department_id, job_id, sum(salary) from employees group by department_id, job_id;
 select department_id, job_id, sum(salary) from employees group by department_id, job_id with rollup;
+
+
+
+
+
+create table group_code(
+    group_code char(3) not null,
+    group_code_name varchar(50) not null,
+    primary key(group_code)
+);
+create table code(
+    group_code char(3) not null,
+    code char(3) not null,
+    code_name varchar(50) not null,
+    use_yn char(1) null,
+    primary key (group_code, code)
+);
+
+
+
+insert into group_code values ( '001', '회원구분');
+insert into group_code values ( '002', '회원상태');
+insert into group_code values ( '003', '주문상태');
+-- 회원구분 공통코드
+insert into code values ('001', '010', '일반회원', 'Y');
+insert into code values ('001', '020', '준회원', 'Y');
+-- insert into code values ('001', '001', '일반회원', 'Y');
+-- 회원상태 공통코드
+insert into code values ('002', '010', 'VIP', 'Y');
+insert into code values ('002', '020', 'GOLD', 'Y');
+insert into code values ('002', '030', 'SIOLVER', 'Y');
+
+
+CREATE TABLE `test`.`users` (
+  `user_id` INT NOT NULL,
+  `user_name` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `user_clsf` CHAR(3) NULL,
+  PRIMARY KEY (`user_id`));
+  
+  
+  -- 프론트에서 회원 구분 목록 요청 ( 회원가입 시에 라디오버튼과 함게 선택할 수 있도록 하기 위해서 )
+-- 공통코드가 없다면 하드코딩 ( 업무적으로 합의 )
+-- 공통코드를 활용하면
+select * from group_code;  -- 001 확인
+select * from code where group_code = '001';
+select code, code_name from code where group_code = '001' and use_yn = 'Y';
+-- 위 결과를 백엔드에서는 프론트에게 내려주면 (json) 프론트는 라디오버튼 등 화면 구성 보여준다. 
+-- 회원은 선택하고 프론트는 선택된 값( 이름, 이메일 등 code = 010 과 함께 )을 전송하게 되고
+-- 백엔드는 users 테이블에 insert 한다. 
+-- insert into users ( user_id, user_name, email, user_clsf ) values ( 100, '백길동', 'back@gildong.com', '010');
+insert into users ( user_id, user_name, email, user_clsf ) values ( 100, '백길동', 'back@gildong.com', '010');
+-- 프론트에서 전체 회원 목록을 필요로 한다. 이 때 회원구분코드외에 회원구분코드명까지 함께 전달한다.
+select user_id, user_name, email, user_clsf,
+         (select code_name from code where group_code = '001' and code = user_clsf ) user_clsf_name from users;
