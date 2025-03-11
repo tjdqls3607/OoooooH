07568da85db841ee7b72812374301193d4bc39ec
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-03-11 15:33:45 +0900

    Add files via upload

--- DataBase/3-11(1).sql
+++ DataBase/3-11(1).sql
@@ -58,6 +58,19 @@ create table code(
 
 
 
+-- 공통코드 테이블을 2개로 분리, 그룹(대분류) 을 표현하는 테이블과 코드ㅍ 및 코드값을 포현하는 테이블 2개로 나눈 방식
+create table group_code( -- 회원구분, 회원상태, .......
+    group_code char(3) not null,  -- 
+    group_code_name varchar(50) not null,
+    primary key(group_code)
+);
+create table code( -- 회원구분의 일반회원, 준회원, .......
+    group_code char(3) not null,
+    code char(3) not null,
+    code_name varchar(50) not null,
+    use_yn char(1) null,
+    primary key (group_code, code)
+);
 insert into group_code values ( '001', '회원구분');
 insert into group_code values ( '002', '회원상태');
 insert into group_code values ( '003', '주문상태');
@@ -68,7 +81,7 @@ insert into code values ('001', '020', '준회원', 'Y');
 -- 회원상태 공통코드
 insert into code values ('002', '010', 'VIP', 'Y');
 insert into code values ('002', '020', 'GOLD', 'Y');
-insert into code values ('002', '030', 'SIOLVER', 'Y');
+insert into code values ('002', '030', 'SILVER', 'Y');
 
 
 CREATE TABLE `test`.`users` (
@@ -93,3 +106,36 @@ insert into users ( user_id, user_name, email, user_clsf ) values ( 100, '백길
 -- 프론트에서 전체 회원 목록을 필요로 한다. 이 때 회원구분코드외에 회원구분코드명까지 함께 전달한다.
 select user_id, user_name, email, user_clsf,
          (select code_name from code where group_code = '001' and code = user_clsf ) user_clsf_name from users;
+         
+         
+-- 공통코드용 function 추가
+-- SET GLOBAL log_bin_trust_function_creators = 1;
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+/* CREATE DEFINER=`root`@`localhost` FUNCTION `fun_code`(
+	p_group_code char(3),
+    p_code char(3)
+) RETURNS varchar(45) CHARSET utf8mb4
+BEGIN
+	declare r_code_name varchar(45);
+    
+    select code_name 
+     into r_code_name
+     from code 
+	where group_code = '001' 
+     and code = user_clsf;
+     
+     
+RETURN r_code_name;
+END
+*/
+
+select user_id, user_name, email, user_clsf, user_state
+        fun_code('001', user_clsf) user_clsf_name,
+        fun_code('002', user_clsf) user_state_name 
+	from users;
+-- scalar sub query 는 subquery returns more than 1 row 오류가 빈번.
+-- 사용자가 복수개의 취미를 가지고ㅓ 그 테이블이 (user_hobby) 일때,
+-- 사용자 1명당 user_hobby 테이블에 값이 없을 수도, 1개일 수도, 복수도 일수도 있다.
+-- 만약 사용자 목록을 추출하면서 사용자의 취미도 같이 추출할 경우 sub query 대신 function 으로 대체한다       
+-- function 안에서 없거나 1개거나 여러개 데이터에 대한 대응 코드를 1개로 return 하는 형태로 많이 만든다.
